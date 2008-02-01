cd4eff6ee0110324f070e62161d8ad6a37cbe568
tailor <pete@petertodd.org>
2008-02-01 15:38:52 +0000

    [tuke @ f0b3d6c49822b6053c8d0a1c043dac310785bfe1] gpl 2.0 -> 3.0

--- Tuke/tests/__init__.py
+++ Tuke/tests/__init__.py
@@ -3,7 +3,7 @@
 # (c) 2004 Lele Gaifax <lele@nautilus.homeip.net>
 # From tailor test suite.
 #
-# This program is made available under the GNU GPL version 2.0 or
+# This program is made available under the GNU GPL version 3.0 or
 # greater. See the accompanying file COPYING for details.
 #
 # This program is distributed WITHOUT ANY WARRANTY; without even the
--- Tuke/tests/meta.py
+++ Tuke/tests/meta.py
@@ -1,7 +1,7 @@
 # vim: tabstop=4 expandtab shiftwidth=4 fileencoding=utf8
 # (c) 2007 Peter Todd <pete@petertodd.org>
 #
-# This program is made available under the GNU GPL version 2.0 or
+# This program is made available under the GNU GPL version 3.0 or
 # greater. See the accompanying file COPYING for details.
 #
 # This program is distributed WITHOUT ANY WARRANTY; without even the
