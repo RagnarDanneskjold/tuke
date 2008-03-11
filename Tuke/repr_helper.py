796d9bae51a58c9568a3f1e616dbd1214a23575e
tailor <pete@petertodd.org>
2008-03-11 12:50:25 +0000

    [tuke @ 7a61c180869f822e13e4429a3f085b1e2c25bc76] explicit merge of 'e32459db503a256ea5953c8685fa0049055c3c84'               and '369e07f12aaadd8693cb06db55a0d2fb2e7a977e'

--- Tuke/repr_helper.py
+++ Tuke/repr_helper.py
@@ -24,6 +24,10 @@ def repr_helper(fn):
     to eval() is relatively involved, with subtle issues like what is the full
     name of the class. This decorator allows the __repr__ function to simply
     return a (args,kwargs) tuple instead, the decorator will handle the rest.
+
+    As a special case, if the repr function needs to change the class name, set
+    '_repr_helper_class_name_override' This is useful if there are alternate,
+    more legible, ways of creating the object.
     """
 
     def f(self):
@@ -34,6 +38,36 @@ def repr_helper(fn):
         if not kwargs:
             kwargs = {}
 
+        class_name = None
+        try:
+            class_name = kwargs['_repr_helper_class_name_override']
+            del kwargs['_repr_helper_class_name_override']
+        except:
+            class_name = self.__class__.__name__
+        module_name = self.__class__.__module__
+
+        # Try to find a shorter representation of the module name.
+        #
+        # Often you'll get stuff like Tuke.element.Element, where the Tuke
+        # __init__ code has a from element import Element line, find those
+        # cases.
+        t = module_name
+        while True:
+            t = t.split('.')[:-1]
+            t = '.'.join(t)
+
+            if not t:
+                break
+
+            m = __import__(t)
+
+            try:
+                if m.__dict__[class_name] is self.__class__:
+                    module_name = t
+                    break
+            except KeyError:
+                continue
+
         # positional arguments are easy, just repr each one
         args = [repr(a) for a in args] 
 
@@ -42,7 +76,6 @@ def repr_helper(fn):
 
         args_str = ','.join(args + kwargs)
 
-        return '%s.%s(%s)' % (self.__class__.__module__,self.__class__.__name__,
-                              args_str)
+        return '%s.%s(%s)' % (module_name,class_name,args_str)
 
     return f
